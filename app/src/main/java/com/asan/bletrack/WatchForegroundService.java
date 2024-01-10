f62493455fa5de3cde421fcf1097d3d5e9ef0364
jy <politics.in.the.pond@gmail.com>
2024-01-10 15:08:25 +0900

    update iBeacon support

--- app/src/main/java/com/asan/bletrack/WatchForegroundService.java
+++ app/src/main/java/com/asan/bletrack/WatchForegroundService.java
@@ -25,6 +25,7 @@ import androidx.core.app.NotificationCompat;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.service.RangedBeacon;
@@ -64,10 +65,12 @@ public class WatchForegroundService extends Service {
         // type.  Do a web search for "setBeaconLayout" to get the proper expression.
         // beaconManager.getBeaconParsers().add(new BeaconParser().
         //        setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
         beaconManager.addRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
                 if (beacons.size() > 0) {
+
                     Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
                 }
             }
